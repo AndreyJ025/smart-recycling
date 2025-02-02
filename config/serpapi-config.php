6b4ed8ea0020ff0deca07cfea70709d4f3ba6118
andreyj <almosaraaj25@gmail.com>
2025-02-03 04:06:39 +0800

    Remove API config file from git tracking

--- config/serpapi-config.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$serpapi_key = '34f935d320a52be6f651ec48ce413f14f94fa97413bcd8a685a0a3ae30931c1f'; // Get from serpapi.com dashboard
--- config/serper-config.php
+++
@@ -1,2 +0,0 @@
-<?php
-$serper_key = '4540ca2db6a6870fde0f66d56d6b61761d764daf'; // Get from https://serper.dev/api-key
